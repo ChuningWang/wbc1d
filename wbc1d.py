a80b31bdc3bff09dd65a1644bab882aa5520a601
Chuning Wang <chuning@esm.rutgers.edu>
2017-03-05 19:41:52 -0500

    add more temporal discretization scheme. final form for wbc1d.

--- wbc1d.py
+++ wbc1d.py
@@ -22,7 +22,7 @@ vbar = wc/beta  # average v-velocity without friction at steady state (beta*v=wc
 
 # methodology switchs
 bc = 'noslip'  # noslip or freeslip
-dmethod = 'leapfrog'  # forward, leapfrog, or mixed
+dmethod = 'trape'  # forward, leapfrog, trape, or AB
 
 # --------------------------------------------------------------------
 # initiate variables
@@ -55,13 +55,46 @@ elif bc=='freeslip':
     zetaw = np.zeros(tn)  # west
     zetae = np.zeros(tn)  # east
 
+# choose finite differencing parameters
+if dmethod == 'forward':
+    alpd = 1.
+    betd = 0.
+    gamd = 0.
+    deld = 1.
+    epsd = 0.
+elif dmethod == 'backward':
+    alpd = 1.
+    betd = 0.
+    gamd = 1.
+    deld = 0.
+    epsd = 0.
+elif dmethod== 'leapfrog':
+    alpd = 0.
+    betd = 1.
+    gamd = 0.
+    deld = 2.
+    epsd = 0.
+elif dmethod == 'trape':
+    alpd = 1.
+    betd = 0.
+    gamd = 0.5
+    deld = 0.5
+    epsd = 0.
+elif dmethod == 'AB':
+    alpd = 1.
+    betd = 0.
+    gamd = 0.
+    deld = 1.5
+    epsd = -0.5
+
 # --------------------------------------------------------------------
 # iterate to solve the equation
 
 # construct linear equation matrix
-A = -2*sparse.eye(xi+2) + \
-    sparse.eye(xi+2, k=1) + \
-    sparse.eye(xi+2, k=-1)
+A = -2*sparse.eye(xi+2)*(1+dt*gamd*gamma) + \
+    sparse.eye(xi+2, k=1)*(1+dt*gamd*(gamma+beta*dx/2)) + \
+    sparse.eye(xi+2, k=-1)*(1+dt*gamd*(gamma-beta*dx/2))
+
 # setup boundary condition
 # no penetration
 A[1, 0], A[1, 2], A[-2, -1], A[-2, -3] = 0, 0, 0, 0
@@ -82,19 +115,39 @@ elif bc=='freeslip':
     A[0, 1] = -2
     A[-1, -2] = -2
 
-for n in range(tn-1):
-    
-    # claculate each term in the equation except d/dt
-    # sverdrup
-    sv = -beta*v[n, 2:-2]
-    # friction
-    fc = -gamma*zeta[n, 2:-2]
+# first step always use trapezoidal
+A0 = -2*sparse.eye(xi+2)*(1+dt*0.5*gamma) + \
+     sparse.eye(xi+2, k=1)*(1+dt*0.5*(gamma+beta*dx/2)) + \
+     sparse.eye(xi+2, k=-1)*(1+dt*0.5*(gamma-beta*dx/2))
+# setup boundary condition
+# no penetration
+A0[1, 0], A0[1, 2], A0[-2, -1], A0[-2, -3] = 0, 0, 0, 0
+A0[1, 1], A0[-2, -2] = 1, 1
 
+# second order
+if bc=='noslip':
+    A0[0, 0] = -1
+    A0[0, 2] = 1
+    A0[-1, -3] = -1
+    A0[-1, -1] = 1
+    A0[0, 1] = 0
+    A0[-1, -2] = 0
+elif bc=='freeslip':
+    A0[0, 0] = 1
+    A0[0, 2] = 1
+    A0[-1, -3] = 1
+    A0[-1, -1] = 1
+    A0[0, 1] = -2
+    A0[-1, -2] = -2
+
+for n in range(tn-1):
+   
     if n==0:
-        # first step always use forward scheme
+        # first step always use trapezoidal scheme
         B = np.zeros(xi+2)
-        B[2:-2] = phi[n, 3:-1] + phi[n, 1:-3] - 2*phi[n, 2:-2] + \
-                  dt*dx*dx*(sv + wc + fc)
+        B[2:-2] = zeta[n, 2:-2]*dx*dx + \
+                  dt*dx*dx*(0.5*wc + \
+                            0.5*(-beta*v[n, 2:-2] + wc - gamma*zeta[n, 2:-2]))
         # setup boundary condition
         # no penetration
         B[1] = phiw[n]
@@ -108,57 +161,37 @@ for n in range(tn-1):
             B[-1] = dx**2*zetae[n]
             
         # solve linear equation
-        phi[n+1, :] = spsolve(A, B)
+        phi[n+1, :] = spsolve(A0, B)
 
         # update v and zeta
         v[n+1, 1:-1] = (phi[n+1, 2:] - phi[n+1, :-2]) / (2*dx)
         zeta[n+1, 1:-1] = (phi[n+1, 2:] + phi[n+1, :-2] - 2*phi[n+1, 1:-1]) / (dx**2)
         continue
     
-    if (dmethod=='forward') | (dmethod=='mixed'):
-        B = np.zeros(xi+2)
-        B[2:-2] = phi[n, 3:-1] + phi[n, 1:-3] - 2*phi[n, 2:-2] + \
-                  dt*dx*dx*(sv + wc + fc)
-        # setup boundary condition
-        # no penetration
-        B[1] = phiw[n]
-        B[-2] = phie[n]
-        # second order
-        if bc=='noslip':
-            B[0] = 2*dx*vw[n]
-            B[-1] = 2*dx*ve[n]
-        elif bc=='freeslip':
-            B[0] = dx**2*zetaw[n]
-            B[-1] = dx**2*zetae[n]
+    # --------------------------------------------------------------------
 
-        # solve linear equation
-        phi1fw = spsolve(A, B)
+    B = np.zeros(xi+2)
+    B[2:-2] = alpd*zeta[n, 2:-2]*dx*dx + \
+              betd*zeta[n-1, 2:-2]*dx*dx + \
+              dt*dx*dx*(gamd*wc + \
+                        deld*(-beta*v[n, 2:-2] + wc - gamma*zeta[n, 2:-2]) + \
+                        epsd*(-beta*v[n-1, 2:-2] + wc - gamma*zeta[n-1, 2:-2]) \
+                       )
 
-    if (dmethod=='leapfrog') | (dmethod=='mixed'):
-        B = np.zeros(xi+2)
-        B[2:-2] = phi[n-1, 3:-1] + phi[n-1, 1:-3] - 2*phi[n-1, 2:-2] + \
-                  2*dt*dx*dx*(sv + wc + fc)
-        # setup boundary condition
-        # no penetration
-        B[1] = phiw[n]
-        B[-2] = phie[n]
-        # second order
-        if bc=='noslip':
-            B[0] = 2*dx*vw[n]
-            B[-1] = 2*dx*ve[n]
-        elif bc=='freeslip':
-            B[0] = dx**2*zetaw[n]
-            B[-1] = dx**2*zetae[n]
+    # setup boundary condition
+    # no penetration
+    B[1] = phiw[n]
+    B[-2] = phie[n]
+    # second order
+    if bc=='noslip':
+        B[0] = 2*dx*vw[n]
+        B[-1] = 2*dx*ve[n]
+    elif bc=='freeslip':
+        B[0] = dx**2*zetaw[n]
+        B[-1] = dx**2*zetae[n]
 
-        phi1lf = spsolve(A, B)
-
-    if (dmethod=='forward'):
-        phi[n+1, :] = phi1fw
-    if (dmethod=='leapfrog'):
-        phi[n+1, :] = phi1lf
-    if (dmethod=='mixed'):
-        phi[n+1, :] = 0.5*(phi1fw+phi1lf)
-
+    # solve linear equation
+    phi[n+1, :] = spsolve(A, B)
     # update v and zeta
     v[n+1, 1:-1] = (phi[n+1, 2:] - phi[n+1, :-2]) / (2*dx)
     zeta[n+1, 1:-1] = (phi[n+1, 2:] + phi[n+1, :-2] - 2*phi[n+1, 1:-1]) / (dx**2)
@@ -166,7 +199,7 @@ for n in range(tn-1):
 # --------------------------------------------------------------------
 # make plots
 
-pltv = 1
+pltv = 0
 if pltv == 1:
     # plot v interactively
     plt.figure()
@@ -177,7 +210,7 @@ if pltv == 1:
             plt.draw()
 
 # plot the output as hovmuller diagram
-plt_hov = 0
+plt_hov = 1
 if plt_hov == 1:
     plt.figure()
     plt.pcolor(t[::5]/24/60/60, x[::10], v[::5, ::10].T, cmap=plt.cm.RdYlBu_r)
@@ -188,7 +221,7 @@ if plt_hov == 1:
     plt.clim(-5, 5)
     cb = plt.colorbar()
     cb.ax.set_ylabel(r'V Velocity')
-    plt.savefig('v_hovmuller_lfrog.png', format='png', dpi=900)
+    plt.savefig('v_' + bc + '_' + dmethod + '.png', format='png', dpi=900)
     plt.close()
 
 
